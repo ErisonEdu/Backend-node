20992042bf2e41bbefb4890789a4a216229072ef
erison <erison.pimentao@gmail,com>
2020-08-14 13:46:51 -0300

    conexão com o backend-node

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const { query } = require('express');
 const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const projects = [];
